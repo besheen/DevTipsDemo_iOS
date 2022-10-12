6e66c41ceafd297b5a73de44986beaaf79b714fd
zhongbisheng <bisheng.zhong@gmail.com>
2022-10-12 15:48:58 +0800

    Add RootViewController

---
+++ DevTipsDemo_iOS/RootViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RootViewController.swift
+//  DevTipsDemo_iOS
+//
+//  Created by Carl on 2022/10/12.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Tip List"
+    }
+}
